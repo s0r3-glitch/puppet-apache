a4bae086a7ec9502cb35e03bf35a2b3d15537c0a
root <root@geygo91c.mylabserver.com>
2021-07-14 19:28:06 +0000

    Refactored module for Hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,9 @@
 #
 # @example
 #   include apache_install
-class apache_install {
+class apache_install(
+  String $install_name,
+  String $install_ensure,
+) {
   include apache_install::install
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,7 @@
 # @summary A short summary of the purpose of this class
 #  Installs the base Apache package.
 #
-class apache_install::install (
-  $install_name   = $apache_install::params::install_name,
-  $install_ensure = $apache_install::params::install_ensure,
-) inherits apache_install::params {
+class apache_install::install {
   package { "${install_name}":
     ensure => $install_ensure,
   }
