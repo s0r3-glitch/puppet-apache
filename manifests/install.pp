debe62ca83b9908c949d6c677004926e1571db7e
root <root@geygo91c.mylabserver.com>
2021-07-14 18:59:26 +0000

    Refactored install class for DEbian family servers

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,11 @@
 # @summary A short summary of the purpose of this class
 #  Installs the base Apache package.
 #
-class apache_install::install {
-  package { 'httpd':
-    ensure => present,
+class apache_install::install (
+  $install_name   = $apache_install::params::install_name,
+  $install_ensure = $apache_install::params::install_ensure,
+) inherits apache_install::params {
+  package { "${install_name}":
+    ensure => $install_ensure,
   }
 }
